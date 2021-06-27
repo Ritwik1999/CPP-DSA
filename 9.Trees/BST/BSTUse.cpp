172318735f574ece91819a3719c3a094bafa6112
Ritwik Neema <ritwikneema651@gmail.com>
2021-06-27 19:58:18 +0530

    BST problems added

--- 9.Trees/BST/BSTUse.cpp
+++ 9.Trees/BST/BSTUse.cpp
@@ -9,9 +9,27 @@
 #include <stack>
 #include <climits>
 #include <cstdlib>
+#include <algorithm>
 #include "BinaryTreeNode.h"
 #include "LLNode.h"
 using namespace std;
+
+class Quad
+{
+public:
+    int minimum;
+    int maximum;
+    bool isBST;
+    int height;
+
+    Quad()
+    {
+        minimum = INT_MAX;
+        maximum = INT_MIN;
+        isBST = true;
+        height = 0;
+    }
+};
 
 void printTree(BinaryTreeNode<int> *root)
 {
@@ -355,6 +373,336 @@ vector<int> *getPath(BinaryTreeNode<int> *root, int data)
         leftOutput->push_back(root->data);
     }
     return leftOutput;
+}
+
+void printPairs(vector<int> &a, int si, int ei, int sum)
+{
+    if (si >= ei)
+    {
+        return;
+    }
+
+    int sumExtremes = a.at(si) + a.at(ei);
+
+    if (sumExtremes == sum)
+    {
+        cout << a.at(si) << " " << a.at(ei) << endl;
+        si++;
+        ei--;
+    }
+
+    else if (sumExtremes > sum)
+    {
+        ei--;
+    }
+
+    else
+    {
+        si++;
+    }
+
+    printPairs(a, si, ei, sum);
+
+    return;
+}
+
+void makeArray(BinaryTreeNode<int> *root, vector<int> &array)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+
+    array.push_back(root->data);
+
+    makeArray(root->left, array);
+    makeArray(root->right, array);
+
+    return;
+}
+
+BinaryTreeNode<int> *getLCABSTRec(BinaryTreeNode<int> *root, int a, int b)
+{
+    if (root == nullptr || root->data == a || root->data == b)
+    {
+        return root;
+    }
+
+    if (root->data < a && root->data < b)
+    {
+        return getLCABSTRec(root->right, a, b);
+    }
+
+    else if (root->data > a && root->data > b)
+    {
+        return getLCABSTRec(root->left, a, b);
+    }
+
+    BinaryTreeNode<int> *leftAns = getLCABSTRec(root->left, a, b);
+    BinaryTreeNode<int> *rightAns = getLCABSTRec(root->right, a, b);
+
+    if (leftAns && rightAns)
+    {
+        return root;
+    }
+
+    if (leftAns)
+    {
+        return leftAns;
+    }
+
+    return rightAns;
+}
+
+Quad largestBSTSubtreeRec(BinaryTreeNode<int> *root)
+{
+    if (root == nullptr)
+    {
+        Quad ans;
+        return ans;
+    }
+
+    Quad leftAns = largestBSTSubtreeRec(root->left);
+    Quad rightAns = largestBSTSubtreeRec(root->right);
+
+    bool overallIsBST = leftAns.isBST && rightAns.isBST && (leftAns.maximum < root->data && rightAns.minimum >= root->data);
+    int overallMinimum = min(root->data, min(leftAns.minimum, rightAns.minimum));
+    int overallMaximum = max(root->data, max(leftAns.maximum, rightAns.maximum));
+    int overallHeight = max(leftAns.height, rightAns.height);
+
+    if (overallIsBST)
+    {
+        overallHeight++;
+    }
+
+    Quad ans;
+    ans.minimum = overallMinimum;
+    ans.maximum = overallMaximum;
+    ans.isBST = overallIsBST;
+    ans.height = overallHeight;
+
+    return ans;
+}
+
+int replaceWithLargerNodesSumRec(BinaryTreeNode<int> *root, int sum)
+{
+    if (root == nullptr)
+    {
+        return sum;
+    }
+
+    sum = replaceWithLargerNodesSumRec(root->right, sum);
+
+    sum += root->data;
+    root->data = sum;
+
+    sum = replaceWithLargerNodesSumRec(root->left, sum);
+
+    return sum;
+}
+
+void printRec(BinaryTreeNode<int> *root, int k, string ans)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+
+    ans = ans + to_string(root->data) + " ";
+
+    if (root->left == nullptr && root->right == nullptr && k == root->data)
+    {
+        cout << ans << endl;
+    }
+
+    printRec(root->left, k - root->data, ans);
+    printRec(root->right, k - root->data, ans);
+
+    return;
+}
+
+void printNodesAtDepthK(BinaryTreeNode<int> *root, int depth)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+
+    if (depth == 0)
+    {
+        cout << root->data << endl;
+        return;
+    }
+
+    printNodesAtDepthK(root->left, depth - 1);
+    printNodesAtDepthK(root->right, depth - 1);
+
+    return;
+}
+
+int printnodesAtDistanceK(BinaryTreeNode<int> *root, int K, int elem)
+{
+    if (root == nullptr)
+    {
+        return -1;
+    }
+
+    if (root->data == elem)
+    {
+        printNodesAtDepthK(root, K);
+        return 0;
+    }
+
+    int leftDepth = printnodesAtDistanceK(root->left, K, elem);
+    if (leftDepth != -1)
+    {
+        if (leftDepth + 1 == K)
+        {
+            cout << root->data << endl;
+        }
+        else
+        {
+            printNodesAtDepthK(root->right, K - leftDepth - 2);
+        }
+        return leftDepth + 1;
+    }
+
+    int rightDepth = printnodesAtDistanceK(root->right, K, elem);
+    if (rightDepth != -1)
+    {
+        if (rightDepth + 1 == K)
+        {
+            cout << root->data << endl;
+        }
+        else
+        {
+            printNodesAtDepthK(root->left, K - rightDepth - 2);
+        }
+        return rightDepth + 1;
+    }
+
+    return -1;
+}
+
+// TODO: Call all the functions defined below from within the main function
+void insertDuplicateNode(BinaryTreeNode<int> *root)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+
+    BinaryTreeNode<int> *duplicateNode = new BinaryTreeNode<int>(root->data);
+    BinaryTreeNode<int> *leftChild = root->left;
+    // BinaryTreeNode<int> *rightChild = root->right;
+
+    root->left = duplicateNode;
+    duplicateNode->left = leftChild;
+
+    insertDuplicateNode(leftChild);
+    insertDuplicateNode(root->right);
+}
+
+void pairSumBinaryTree(BinaryTreeNode<int> *root, int sum)
+{
+
+    vector<int> array;
+
+    makeArray(root, array);
+
+    sort(array.begin(), array.end());
+
+    printPairs(array, 0, array.size() - 1, sum);
+
+    return;
+}
+
+int getLCABinaryTree(BinaryTreeNode<int> *root, int a, int b)
+{
+    if (root == nullptr)
+    {
+        return -1;
+    }
+
+    if (root->data == a || root->data == b)
+    {
+        return root->data;
+    }
+
+    int leftAns = getLCABinaryTree(root->left, a, b);
+    int rightAns = getLCABinaryTree(root->right, a, b);
+
+    if (leftAns == -1 && rightAns != -1)
+    {
+        return rightAns;
+    }
+
+    if (leftAns != -1 && rightAns == -1)
+    {
+        return leftAns;
+    }
+
+    if (leftAns == rightAns)
+    {
+        if (leftAns == -1)
+        {
+            return -1;
+        }
+
+        return leftAns;
+    }
+
+    return root->data;
+}
+
+int getLCABST(BinaryTreeNode<int> *root, int val1, int val2)
+{
+    BinaryTreeNode<int> *LCANode = getLCABSTRec(root, val1, val2);
+    return (LCANode == nullptr ? -1 : LCANode->data);
+}
+
+// Given a Binary tree, find the largest BST subtree. That is, you need to find the BST with maximum height in the given binary tree. You have to return the height of largest BST.
+int largestBSTSubtree(BinaryTreeNode<int> *root)
+{
+    Quad q = largestBSTSubtreeRec(root);
+    return q.height;
+}
+
+// Given a binary search tree, you have to replace each node's data with the sum of all nodes which are greater or equal than it. You need to include the current node's data also.
+// That is, if in given BST there is a node with data 5, you need to replace it with sum of its data (i.e. 5) and all nodes whose data is greater than or equal to 5.
+void replaceWithLargerNodesSum(BinaryTreeNode<int> *root)
+{
+    replaceWithLargerNodesSumRec(root, 0);
+}
+
+// For a given Binary Tree of type integer and a number K, print out all root-to-leaf paths where the sum of all the node data along the path is equal to K.
+void rootToLeafPathsSumToK(BinaryTreeNode<int> *root, int k)
+{
+    string ans = "";
+    printRec(root, k, ans);
+    return;
+}
+
+// You are given a Binary Tree of type integer, a target node, and an integer value K.
+// Print the data of all nodes that have a distance K from the target node. The order in which they would be printed will not matter.
+void nodesAtDistanceK(BinaryTreeNode<int> *root, int node, int k)
+{
+    int ans = printnodesAtDistanceK(root, k, node);
+    return;
+}
+
+// Given a binary search tree and an integer S, find pair of nodes in the BST which sum to S. You can use extra space of the order of O(log n).
+void printNodesSumToS(BinaryTreeNode<int> *root, int sum) {
+    
+    vector<int> array;
+    
+    makeArray(root, array);
+    
+    sort(array.begin(), array.end());
+    
+    printPairs(array, 0, array.size()-1, sum);
+    
+    return;
 }
 
 // Sample input1: 1 2 3 4 5 6 7 -1 -1 -1 -1 8 9 -1 -1 -1 -1 -1 -1 (Not a BST)
