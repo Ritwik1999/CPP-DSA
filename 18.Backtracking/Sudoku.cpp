20923f3bc076cf7751f92f53a196c2235ef66f97
Ritwik Neema <ritwikneema651@gmail.com>
2021-07-27 15:41:25 +0530

    Sudoku comments added

--- 18.Backtracking/Sudoku.cpp
+++ 18.Backtracking/Sudoku.cpp
@@ -1,3 +1,36 @@
+/*
+Write a program to find a solution to a sudoku puzzle.
+The input will consist of puzzles with exactly one solution.
+
+Input Format:
+The input consists of one test case.
+Each test case will consist of nine lines with nine digits on each line.
+There will not be any blanks between the digits.
+The digits '1' through '9' will represent a number and the digit '0' will represent a blank square.
+
+Sample Input 1:
+023456789
+406789123
+780123456
+234067891
+567801234
+891230567
+345678012
+678912305
+912345670
+
+Sample Output 1:
+123456789
+456789123
+789123456
+234567891
+567891234
+891234567
+345678912
+678912345
+912345678
+*/
+
 #include <iostream>
 using namespace std;
 
