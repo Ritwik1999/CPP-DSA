8970170804292557a85e0a20b56b244704bbfa22
Ritwik Neema <ritwikneema651@gmail.com>
2022-07-14 18:01:01 +0530

    char array pointer changes - 2

--- 1.Pointers/char_array_pointer.cpp
+++ 1.Pointers/char_array_pointer.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 int main()
 {
-    char StringLiteral[] = "Ritzy 123";
+    char StringLiteral[] = "Ritzy 123";     // (A)
     cout << StringLiteral << endl;
     cout << &StringLiteral[0] << endl;
     cout << StringLiteral[0] << endl;
-    char *StringPointer = "Ritzy 123";
+    char *StringPointer = "Ritzy 123";      // (B)
 
     /*
-        Line 7 would raise a warning, discouraging it's use.
-        This is because in line 6, we own the contents of the array,
-        and hence we can modify it, but in Line 7, it is just a pointer to a 
-        (constant) string literal, we do not own it.
+        Line B would raise a warning, discouraging it's use.
+        Whereas line A is fine. This is because in line A, we own the contents of the array,
+        and hence we can modify it, but in line B, it is just a pointer to a 
+        (constant) string literal, and we do not own it.
 
         In other words, whenever we make a pointer,
         we point it to the address of something that actually exists in the memory,
-        but in case of line 7, the string literal doesn't exist at all,
-        we define it temporarily, and point towards it.
+        but in case of line B, the string literal doesn't exist at all,
+        we define it temporarily, and point to it.
         This temporary memory may be overwritten anytime.
         
-        Hence, line 7 code is dangerous, and unpredictable.
+        Hence, line B code is dangerous, and unpredictable.
     */
 
     return 0;
