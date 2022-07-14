63016cc6a7d959e0854b214530498f53c105456c
Ritwik Neema <ritwikneema651@gmail.com>
2022-07-14 16:21:13 +0530

    info added

--- 1.Pointers/pointers_operator.cpp
+++ 1.Pointers/pointers_operator.cpp
@@ -7,14 +7,18 @@ int main()
     double *dp = &d;
 
     cout << dp << endl;
+    // The two statements below may output different values
+    cout << "Size of double variable on this systems compiler is: " << sizeof(d) << endl;
+    cout << "Size of double pointer on this systems compiler is: " << sizeof(dp) << endl;
     
     dp = dp + 2; // skips 2*(sizeof(double)), ahead.
     cout << dp << endl;
 
-    dp = dp - 2;
+    dp = dp - 2; // skips 2*(sizeof(double)), behind.
     cout << dp << endl;
 
     // You can even perform comparisons on pointers of similar type
+    cout << (dp == dp) << endl;
 
     return 0;
 }
