b9e0d5e8b089c2eab69abd81978e0f10e5f96f77
Ritwik Neema <ritwikneema651@gmail.com>
2022-07-14 20:26:16 +0530

    2d dynamic array

--- 2.Dynamic Allocation/dynamic_2darray.cpp
+++ 2.Dynamic Allocation/dynamic_2darray.cpp
@@ -12,18 +12,27 @@
 #include <iostream>
 using namespace std;
 
+// TIP: Debug the program for better understanding
 int main()
 {
     int m, n;
     cin >> m >> n;
-    int **p = new int *[m];
+    int **p = new int *[m];     // p would just store a single address, which would be the base address of an array of integer pointers
     for (int i = 0; i < m; i++)
     {
-        p[i] = new int[i + 1];
+        p[i] = new int[i + 1];  // p[i] or *(p+i) would contain the base address of an array of integers.
         for (int j = 0; j < n; j++)
         {
             cin >> p[i][j];
         }
+    }
+
+    // output the 2d array
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << p[i][j] << " ";
+        }
+        cout << endl;
     }
 
     for (int i = 0; i < m; i++)
